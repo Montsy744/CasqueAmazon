827913722f788d47b5e7d5e9408bb793f9690fc2
Hugo Straseele <straseelehugo@gmail.com>
2025-09-10 14:43:42 +0200

    ajout du début des animation du lancement de la page

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,16 @@
 /* code des btn de la prtie header*/
 const menu = document.getElementById('menu');
+const compte = document.getElementById('compte');
+const panier = document.getElementById('panier');
 
-menu.addEventListener('click', () => {
+menu.addEventListener('click', () => { 
   window.open('https://www.amazon.fr/');
 })
+
+compte.addEventListener('click', () => {
+  window.open('https://www.amazon.fr/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.fr%2F%3Flanguage%3Dfr_FR%26currency%3DEUR%26ref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=frflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0');
+});
+
+panier.addEventListener('click', () => {
+  window.open('https://www.amazon.fr/-/en/gp/cart/view.html?ref_=nav_cart');
+});
